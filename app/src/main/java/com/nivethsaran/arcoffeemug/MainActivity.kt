e4f6b4ef2d3c1021880b5f94ddda3f9edda085b3
Niveth_Saran <nivethsaran@gmail.com>
2020-08-12 21:54:02 +0530

    Initial commit

---
+++ app/src/main/java/com/nivethsaran/arcoffeemug/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.nivethsaran.arcoffeemug
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.collision.Box
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    
+}
