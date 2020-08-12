17d62b62c22535567319b4ab436ee459affa480f
Niveth_Saran <nivethsaran@gmail.com>
2020-08-12 22:00:02 +0530

    add models to the scene

--- app/src/main/java/com/nivethsaran/arcoffeemug/MainActivity.kt
+++ app/src/main/java/com/nivethsaran/arcoffeemug/MainActivity.kt
@@ -19,9 +19,88 @@ import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var arFragment: ArFragment
+    val viewNodes = mutableListOf<Node>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        arFragment= fragment as ArFragment
+
+        arFragment.setOnTapArPlaneListener{hitResult,x,y ->
+            loadModel{modelRenderable, viewRenderable ->
+                addNodeToScene(hitResult.createAnchor(),modelRenderable,viewRenderable)
+            }
+        }
+
     }
-    
+
+
+    private fun addNodeToScene(
+        anchor: Anchor,
+        modelRenderable: ModelRenderable,
+        viewRenderable: ViewRenderable
+    ) {
+        val anchorNode = AnchorNode(anchor)
+        val modelNode = TransformableNode(arFragment.transformationSystem).apply {
+            renderable = modelRenderable
+            setParent(anchorNode)
+            arFragment.arSceneView.scene.addChild(anchorNode)
+            select()
+        }
+        val viewNode = Node().apply {
+            renderable = null
+            setParent(modelNode)
+            val box = modelNode.renderable?.collisionShape as Box
+            localPosition = Vector3(0f, box.size.y, 0f)
+            (viewRenderable.view as Button).setOnClickListener {
+                arFragment.arSceneView.scene.removeChild(anchorNode)
+                viewNodes.remove(this)
+            }
+        }
+        viewNodes.add(viewNode)
+        modelNode.setOnTapListener { _, _ ->
+            if(!modelNode.isTransforming) {
+                if(viewNode.renderable == null) {
+                    viewNode.renderable = viewRenderable
+                } else {
+                    viewNode.renderable = null
+                }
+            }
+        }
+    }
+
+    private fun createDeleteButton(): Button {
+        return Button(this).apply {
+            text = "REMOVE"
+            setBackgroundColor(Color.CYAN)
+            setTextColor(Color.WHITE)
+        }
+    }
+
+
+    private fun loadModel(callback: (ModelRenderable, ViewRenderable) -> Unit) {
+        val modelRenderable = ModelRenderable.builder()
+            .setSource(this, R.raw.coffeecup)
+            .build()
+
+        val viewRenderable = ViewRenderable.builder()
+            .setView(this, createDeleteButton())
+            .build()
+
+        CompletableFuture.allOf(modelRenderable, viewRenderable)
+            .thenAccept {
+                callback(modelRenderable.get(), viewRenderable.get())
+            }
+            .exceptionally {
+                Toast.makeText(this, "Error loading model: $it", Toast.LENGTH_LONG).show()
+                null
+            }
+    }
+
+
+
 }
